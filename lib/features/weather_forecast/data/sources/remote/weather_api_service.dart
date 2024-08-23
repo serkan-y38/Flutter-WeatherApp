e824ffebbab0664083c7f4ba728c6b78fc8a55d6
Serkan <yilmzsrkn@gmail.com>
2024-08-23 20:25:51 +0300

    changed android app icon, minor changes

--- lib/features/weather_forecast/data/sources/remote/weather_api_service.dart
+++ lib/features/weather_forecast/data/sources/remote/weather_api_service.dart
@@ -1,7 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:retrofit/retrofit.dart';
 import 'package:weather/core/constant/api_key.dart';
-import 'package:weather/core/constant/constents.dart';
+import 'package:weather/core/constant/constants.dart';
 import 'package:weather/features/weather_forecast/data/model/remote/weather_forecast_response.dart';
 import '../../model/remote/currect_weather_response.dart';
 
